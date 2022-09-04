9bc6ac31376885a2f0a18b0df33f54e9cd769a96
RskullW <mailcom.99@mail.ru>
2022-09-04 15:37:55 +0300

    first commit

---
+++ Manager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApplication1.Properties;
+
+namespace ConsoleApplication1
+{
+    public class Manager
+    {
+        public void Display(string message)
+        {
+            Console.WriteLine(message);
+        }
+        private void DisplayStartMenu(ref ushort numberOfItemsMenu)
+        {
+        }
+
+        private void DisplayMenu()
+        {
+        }
+        public void StartPause()
+        {
+            Display("Enter any character to continue");
+            var temp = Console.ReadLine();
+            Console.Clear();
+        }
+        // MENU
+        public void OpenStartMenu()
+        {
+        }
+        
+        public void OpenMenu()
+        {
+            
+        }
+        public void CloseMenu()
+        {
+            Display("Menu closed!");
+        }
+        // CREATION
+        public Manager()
+        {
+            
+        }
+        // CORRECTNESS
+        public ushort CorrectInput(ushort minValue = 0, ushort maxValue = 1)
+        {
+            string numberString = "0";
+            ushort number;
+            Display("Enter a number from " + minValue.ToString() + " to " + maxValue.ToString());
+
+            numberString = Console.ReadLine();
+            
+            number = ushort.Parse(numberString);
+
+            if (number < minValue || number > maxValue)
+            {
+                number = 0;
+            }
+
+            return number;
+        }
+        private bool CheckForExistence()
+        {
+            return true;
+        }
+    }
+    
+    /*
+     * ‒ задание свойств каждого объекта; (хотя бы по одному объекту 
+на не абстрактный класс);
+‒ вывод свойств объекта;
+‒ выполнение методов объекта;
+‒ вывод имени класса объекта
+     */
+}
--- Program.cs
+++ Program.cs
@@ -6,16 +6,11 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        private static List<Capybara> _capybaras;
-        private static ManagerCapybaras _managerCapybaras;
-
         public static void Main(string[] args)
         {
             try
             {
-                _capybaras = new List<Capybara>(1);
-                _managerCapybaras = new ManagerCapybaras(_capybaras);
-                _managerCapybaras.OpenMenu();
+                
             }
 
             catch (Exception exception)
@@ -25,11 +20,3 @@ namespace ConsoleApplication1
         }
     }
 }
-
-/*
-TODO: need to create a menu.
-‒ задание параметров конструируемого объекта;
-‒ вывод свойств объекта;
-‒ выполнение статического метода;
-‒ выполнение методов объекта.
-*/
