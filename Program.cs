f4382c851e05f0339c2bd0e862384eae3892f209
RskullW <mailcom.99@mail.ru>
2022-09-22 03:36:42 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,38 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication1
 {
-    internal class Program
+    class GCProgram
     {
-        public static void Main(string[] args)
+        private const long maxGarbage = 1000;
+        static void Main()
         {
+            GCProgram myGCCol = new GCProgram();
+            Console.WriteLine("The highest generation is {0}", GC.MaxGeneration);
+            myGCCol.MakeSomeGarbage();
+            Console.WriteLine("Generation: {0}", GC.GetGeneration(myGCCol));
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            GC.Collect(0); 
+            Console.WriteLine("Generation: {0}", GC.GetGeneration(myGCCol));
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            GC.Collect(2);
+            Console.WriteLine("Generation: {0}", GC.GetGeneration(myGCCol));
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            Console.Read();
+        }
+        void MakeSomeGarbage()
+        {
+            Version vt;
+            StringBuilder stringBuilder;
+            
+            for (int i = 0; i < maxGarbage; i++)
+            {
+                vt = new Version();
+                stringBuilder = new StringBuilder();
+            }
         }
     }
 }
